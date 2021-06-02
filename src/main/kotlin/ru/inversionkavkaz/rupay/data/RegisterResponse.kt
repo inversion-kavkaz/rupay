0ef6709c01763d757c21878cf53a053ba83ac047
bvv <bvv@inversion-kavkaz.ru>
2021-06-02 13:45:48 +0300

    Initial commit

--- src/main/kotlin/ru/inversionkavkaz/rupay/data/RegisterResponse.kt
+++ src/main/kotlin/ru/inversionkavkaz/rupay/data/RegisterResponse.kt
@@ -3,7 +3,7 @@ package ru.inversionkavkaz.rupay.data
 import java.util.*
 
 data class RegisterResponse(val reqID : Long,
-                            var transactionID: Long,
-                            var statusDate: Date,
-                            var statusCode: Int,
-                            var statusMessage: String)
+                            val transactionID: Long,
+                            val statusDate: Date,
+                            val statusCode: Int,
+                            val statusMessage: String)
